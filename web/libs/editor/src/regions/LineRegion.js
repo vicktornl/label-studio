3b183f8595cef8a708364756d425804eefc1da4d
Martijn Jacobs <maerteijn@gmail.com>
2025-01-02 12:56:40 +0100

    Implement a Line region, based on the PolygonRegion

---
+++ web/libs/editor/src/regions/LineRegion.js
@@ -0,0 +1,33 @@
+import { types } from "mobx-state-tree";
+import Registry from "../core/Registry";
+import { PolygonRegionModel, HtxPolygon } from "./PolygonRegion";
+
+const Model = types
+  .model({
+    type: "lineregion",
+    minNumPoints: types.optional(types.integer, 2),
+    maxNumPoints: types.optional(types.integer, 2),
+    canAddPointsAfterClosure: types.optional(types.boolean, false),
+  })
+  .views((self) => ({
+    get maxPointsReached() {
+      return self.maxNumPoints && self.points.length >= self.maxNumPoints;
+    },
+  }));
+
+const LineRegionModel = types.compose(
+  "LineRegionModel",
+  PolygonRegionModel,
+  Model,
+);
+
+const HtxLine = HtxPolygon;
+
+Registry.addTag("lineregion", LineRegionModel, HtxLine);
+Registry.addRegionType(
+  LineRegionModel,
+  "image",
+  (value) => value.results[0].type == "line",
+);
+
+export { LineRegionModel, HtxLine };
